d8c75fc4bdf6cda8a99fbc41ffc7291f0ee5f479
Lucas :) <168363697+ikzLucas@users.noreply.github.com>
2025-10-23 13:11:43 -0600

    more options- linewidth, colors, scrolloff

--- lua/options.lua
+++ lua/options.lua
@@ -1,11 +1,18 @@
 local options = {
+   -- true color support
+   termguicolors = true,
+   -- ignore case in search
+   ignorecase = true,
 	-- line numbers
 	number = true,
 	relativenumber = true,
+   numberwidth = 2,
 	-- three \t chars when pressing <tab>, replaced with whitespace
 	tabstop = 3,
 	shiftwidth = 3,
 	expandtab = true,
+   -- scrolloff (lines always visible above/below cursor)
+   scrolloff = 8,
 }
 
 -- For each option in options array, append vim.o to the option
