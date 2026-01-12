4d6aec7aecd5dfcbcab62e35c4a6e68c5be0fd51
Lucas :D <168363697+ikzLucas@users.noreply.github.com>
2026-01-11 17:34:06 -0700

    Formataroo

--- init.lua
+++ init.lua
@@ -7,70 +7,70 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 vim.diagnostic.config({ jump = { float = true } })
-vim.g.netrw_liststyle = 3     -- netrw tree style by default
+vim.g.netrw_liststyle = 3 -- netrw tree style by default
 vim.g.have_nerd_font = true
 
 -- Gentoo files fix
-vim.opt.rtp:append('/usr/share/vim/vimfiles')
+vim.opt.rtp:append("/usr/share/vim/vimfiles")
 
 -- highlight text when yanking
 vim.api.nvim_create_autocmd("TextYankPost", {
-   callback = function()
-      vim.hl.on_yank {
-         higroup = "Visual",
-         timeout = 300,
-      }
-   end,
+	callback = function()
+		vim.hl.on_yank({
+			higroup = "Visual",
+			timeout = 300,
+		})
+	end,
 })
 
 ---- KEYBINDS ----
-keybind("n", "<Leader>ex", "<Cmd>Ex %:p:h<CR>")       -- Open netrw
-keybind("n", "<Leader>lex", "<Cmd>Lex %:p:h<CR>")     -- Split netrw left
-keybind("n", "<Leader>rex", "<Cmd>Rex<CR>")           -- Return to previous netrw window
-keybind("n", "<Leader>vs", "<CMD>vs<CR>")             -- Leader+vs vertical split - this is also <C-w>v
-keybind("n", "<Leader>lf", vim.lsp.buf.format)        -- LSP format code
-keybind("n", "<Leader>d", vim.diagnostic.open_float)  -- LSP diagnostic - this is also <C-w>d
-keybind("n", "<Esc>", "<Cmd>nohlsearch<CR>")          -- Clear highlights from search when pressing ESC
-keybind("n", "<C-h>", "<C-w><C-h>", { desc = 'Move focus to the left window' })
-keybind("n", "<C-l>", "<C-w><C-l>", { desc = 'Move focus to the right window' })
-keybind("n", "<C-j>", "<C-w><C-j>", { desc = 'Move focus to the lower window' })
-keybind("n", "<C-k>", "<C-w><C-k>", { desc = 'Move focus to the upper window' })
+keybind("n", "<Leader>ex", "<Cmd>Ex %:p:h<CR>") -- Open netrw
+keybind("n", "<Leader>lex", "<Cmd>Lex %:p:h<CR>") -- Split netrw left
+keybind("n", "<Leader>rex", "<Cmd>Rex<CR>") -- Return to previous netrw window
+keybind("n", "<Leader>vs", "<CMD>vs<CR>") -- Leader+vs vertical split - this is also <C-w>v
+keybind("n", "<Leader>lf", vim.lsp.buf.format) -- LSP format code
+keybind("n", "<Leader>d", vim.diagnostic.open_float) -- LSP diagnostic - this is also <C-w>d
+keybind("n", "<Esc>", "<Cmd>nohlsearch<CR>") -- Clear highlights from search when pressing ESC
+keybind("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+keybind("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+keybind("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+keybind("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 -- Options for neovim - see :help option-list
 local options = {
-   ---- EDITOR OPTIONS ----
-   confirm = true,         -- confirm for actions like quit
-   mouse = 'a',           -- mouse for resizing stuff
+	---- EDITOR OPTIONS ----
+	confirm = true, -- confirm for actions like quit
+	mouse = "a", -- mouse for resizing stuff
 
-   ---- APPEARANCE ----
-   termguicolors = true,
-   background = "light",
-   signcolumn = "yes",
-   splitright = true,
-   splitbelow = true,
+	---- APPEARANCE ----
+	termguicolors = true,
+	background = "light",
+	signcolumn = "yes",
+	splitright = true,
+	splitbelow = true,
 
-   -- LINE NUMBERS
-   number = true,
-   relativenumber = true,
-   numberwidth = 2,
-   scrolloff = 11,
-   cursorline = true,
+	-- LINE NUMBERS
+	number = true,
+	relativenumber = true,
+	numberwidth = 2,
+	scrolloff = 11,
+	cursorline = true,
 
-   ---- SEARCH ----
-   ignorecase = true,
-   smartcase = true,
+	---- SEARCH ----
+	ignorecase = true,
+	smartcase = true,
 
-   ---- TYPING ----
-   tabstop = 3, -- three \t chars when pressing <tab>, replaced with whitespace
-   shiftwidth = 3,
-   expandtab = true,
-   autocomplete = true,    -- enable 0.12.0 native autocompletions
-   autocompletedelay = 50, -- slight delay before autocomplete begins
-   inccommand = 'split'    -- preview subsitutions live
+	---- TYPING ----
+	tabstop = 3, -- three \t chars when pressing <tab>, replaced with whitespace
+	shiftwidth = 3,
+	expandtab = true,
+	autocomplete = true, -- enable 0.12.0 native autocompletions
+	autocompletedelay = 50, -- slight delay before autocomplete begins
+	inccommand = "split", -- preview subsitutions live
 }
 
 -- For each option in options array, append vim.o to the option
 for k, v in pairs(options) do
-   vim.o[k] = v
+	vim.o[k] = v
 end
 
 -- enable plugins and indentation for autodetected filetypes
@@ -78,30 +78,30 @@ vim.cmd.filetype("plugin indent on")
 
 ---- PLUGINS ----
 vim.pack.add({
-   { src = "https://github.com/kylechui/nvim-surround" },
-   { src = "https://github.com/miikanissi/modus-themes.nvim" },
-   { src = "https://github.com/neovim/nvim-lspconfig" },
-   { src = "https://github.com/mason-org/mason.nvim" },
-   { src = "https://github.com/mason-org/mason-lspconfig.nvim" },
+	{ src = "https://github.com/kylechui/nvim-surround" },
+	{ src = "https://github.com/miikanissi/modus-themes.nvim" },
+	{ src = "https://github.com/neovim/nvim-lspconfig" },
+	{ src = "https://github.com/mason-org/mason.nvim" },
+	{ src = "https://github.com/mason-org/mason-lspconfig.nvim" },
 })
 
 require("nvim-surround").setup()
 require("modus-themes").setup({
-   style = "auto",
-   variant = "tinted", -- `default`, `tinted`, `deuteranopia`, and `tritanopia`
-   transparent = false,
-   dim_inactive = true,
-   hide_inactive_statusline = false,
-   line_nr_column_background = true,
-   sign_column_background = true,
-   styles = {
-      comments = { italic = true },
-      keywords = { italic = true },
-      functions = {},
-      variables = {},
-   },
+	style = "auto",
+	variant = "tinted", -- `default`, `tinted`, `deuteranopia`, and `tritanopia`
+	transparent = false,
+	dim_inactive = true,
+	hide_inactive_statusline = false,
+	line_nr_column_background = true,
+	sign_column_background = true,
+	styles = {
+		comments = { italic = true },
+		keywords = { italic = true },
+		functions = {},
+		variables = {},
+	},
 
-   	--- You can override specific color groups to use other groups or a hex color
+	--- You can override specific color groups to use other groups or a hex color
 	--- Function will be called with a ColorScheme table
 	--- Refer to `extras/lua/modus_operandi.lua` or `extras/lua/modus_vivendi.lua` for the ColorScheme table
 	---@param colors ColorScheme
@@ -117,26 +117,56 @@ require("modus-themes").setup({
 vim.cmd("colorscheme modus")
 
 ---- LSP ----
-require("mason").setup({
-   ui = {
-      icons = {
-         package_installed = "✓",
-         package_pending = "➜",
-         package_uninstalled = "✗"
-      }
-   }
+
+-- from example in :h LspAttach
+vim.api.nvim_create_autocmd("LspAttach", {
+	group = vim.api.nvim_create_augroup("my.lsp", {}),
+	callback = function(args)
+		local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+
+		if client:supports_method("textDocument/completion") then
+			vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
+		end
+	end,
 })
-require("mason-lspconfig").setup {
-   ensure_installed = { "lua_ls", "ansiblels", "clangd", "powershell_es", "docker_language_server", "docker_compose_language_service" },
-}
-vim.lsp.config("lua_ls",
-   {
-      settings = {
-         Lua = {
-            workspace = {
-               library = vim.api.nvim_get_runtime_file("", true),
-            }
-         }
-      }
-   }
-)
+vim.cmd("set completeopt+=noselect")
+
+-- filetypes
+vim.filetype.add({
+	pattern = {
+		["%*ansible*.y*ml"] = "yaml.ansible",
+		["%*docker-compose*.yml"] = "yaml.docker-compose",
+		["docker-compose.yaml"] = "yaml.docker-compose",
+		["compose.yml"] = "yaml.docker-compose",
+		["compose.yaml"] = "yaml.docker-compose",
+	},
+})
+
+require("mason").setup({
+	ui = {
+		icons = {
+			package_installed = "✓",
+			package_pending = "➜",
+			package_uninstalled = "✗",
+		},
+	},
+})
+require("mason-lspconfig").setup({
+	ensure_installed = {
+		"lua_ls",
+		"ansiblels",
+		"clangd",
+		"powershell_es",
+		"docker_language_server",
+		"docker_compose_language_service",
+	},
+})
+vim.lsp.config("lua_ls", {
+	settings = {
+		Lua = {
+			workspace = {
+				library = vim.api.nvim_get_runtime_file("", true),
+			},
+		},
+	},
+})
